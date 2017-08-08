144fedd98cef6d7ffa98e270324900564d12d620
Okan Fidan <30366273+kent0418@users.noreply.github.com>
2017-08-08 10:24:55 +0300

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,29 +1,3 @@
-<?php
-	//header("HTTP/1.1 301");
-	header("Content-Type:image/jpeg");
-	header_remove('Content-Type');
-	//header("Location: http://i.imgur.com/$id.jpg");
-  	//exit();
-	
-	//if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])){
-	//  	header('Last-Modified: '.@$_SERVER['HTTP_IF_MODIFIED_SINCE'],true,304);
-	//  	exit;
-	//}
-	//header("Cache-Control:public, max-age=0");
-    //header('Expires: ' . gmdate('D, d M Y H:i:s', time() + (60*60*24*45)) . ' GMT');
-    //header('Last-Modified: ' . gmdate('D, d M Y H:i:s', time() - (60*60*24*45)) . ' GMT');
-	//header("Content-Type:image/jpeg");
-	//$ch = curl_init(); 
-	//curl_setopt($ch, CURLOPT_URL, "http://tinyurl.com/$id");
-	//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	//$output = curl_exec($ch);
-	//curl_close($ch);
-	//echo $output;
-	//exit;
-
-	$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-?>
 <!DOCTYPE html>
 <html>
 	<head>
