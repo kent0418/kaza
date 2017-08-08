1dbc4cd7b2899f893675020c2566a9068c9b7b5e
Okan Fidan <30366273+kent0418@users.noreply.github.com>
2017-08-08 10:22:09 +0300

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,23 +1,49 @@
 <?php
-	$flink = 'http://'.generate_name(rand(6,8)).".com/".$id;
+	//header("HTTP/1.1 301");
+	header("Content-Type:image/jpeg");
+	header_remove('Content-Type');
+	//header("Location: http://i.imgur.com/$id.jpg");
+  	//exit();
+	
+	//if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])){
+	//  	header('Last-Modified: '.@$_SERVER['HTTP_IF_MODIFIED_SINCE'],true,304);
+	//  	exit;
+	//}
+	//header("Cache-Control:public, max-age=0");
+    //header('Expires: ' . gmdate('D, d M Y H:i:s', time() + (60*60*24*45)) . ' GMT');
+    //header('Last-Modified: ' . gmdate('D, d M Y H:i:s', time() - (60*60*24*45)) . ' GMT');
+	//header("Content-Type:image/jpeg");
+	//$ch = curl_init(); 
+	//curl_setopt($ch, CURLOPT_URL, "http://tinyurl.com/$id");
+	//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	//$output = curl_exec($ch);
+	//curl_close($ch);
+	//echo $output;
+	//exit;
+
+	$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-	<script src="/<?php echo generate_name(rand(10,20)) ?>.js"></script>
-	<meta property="og:type" content="website" />
-	<meta property="og:title" content="<?php echo $id; ?>" /> 
-	<meta property="og:url" content="<?php echo $flink; ?>" />
-	<script src="<?php echo $flink; ?>.js"></script>
-</head>
-<body>
-<?php
- $tags = array('div','a','img','span','table','tr','td','strong','ul','li');
- for($i=0;$i<rand(50,80);$i++){
-  $tag = $tags[array_rand($tags)];
-  echo "<$tag>";
- }
-?>
-</body>
+	<head>
+		<title><?php echo $id; ?></title>
+		<meta property="og:type" content="video.movie" />
+		<meta property="og:title" content="<?php echo $id; ?>" />
+		<meta property="og:description" content="." />
+		<meta property="og:image" content="http://tinyurl.com/<?php echo $id; ?>" />
+		<meta property="og:image:secure_url" content="https://tinyurl.com/<?php echo $id; ?>" />
+		<meta property="og:image:type" content="image/jpeg" />
+		<meta property="og:image:width" content="800" />
+	    <meta property="og:image:height" content="420" />
+	</head>
+	<body>
+	<?php
+	 $tags = array('div','a','img','span','table','tr','td','strong','ul','li');
+	 for($i=0;$i<rand(5,10);$i++){
+	  $tag = $tags[array_rand($tags)];
+	  echo "<$tag>";
+	 }
+	?>
+	</body>
 </html>
