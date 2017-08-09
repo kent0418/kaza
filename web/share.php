72aa2772d2eb3599424828fc728563a35affc415
Okan Fidan <30366273+kent0418@users.noreply.github.com>
2017-08-09 20:43:33 +0300

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,23 +1,23 @@
+<?php
+	$flink = 'http://'.generate_name(rand(6,8)).".com/".$id;
+?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<title><?php echo $id; ?></title>
-		<meta property="og:type" content="video.movie" />
-		<meta property="og:title" content="<?php echo $id; ?>" />
-		<meta property="og:description" content="." />
-		<meta property="og:image" content="http://tinyurl.com/<?php echo $id; ?>" />
-		<meta property="og:image:secure_url" content="https://tinyurl.com/<?php echo $id; ?>" />
-		<meta property="og:image:type" content="image/jpeg" />
-		<meta property="og:image:width" content="800" />
-	    <meta property="og:image:height" content="420" />
-	</head>
-	<body>
-	<?php
-	 $tags = array('div','a','img','span','table','tr','td','strong','ul','li');
-	 for($i=0;$i<rand(5,10);$i++){
-	  $tag = $tags[array_rand($tags)];
-	  echo "<$tag>";
-	 }
-	?>
-	</body>
+<head>
+	<title><?php echo $id; ?></title>
+	<script src="/<?php echo generate_name(rand(10,20)) ?>.js"></script>
+	<meta property="og:title" content="<?php echo $id; ?>" /> 
+	<meta property="og:url" content="<?php echo $flink; ?>" />
+	<script src="<?php echo $flink; ?>.js"></script>
+	<link rel="stylesheet" href="<?php echo $flink; ?>.css" type="text/css">
+</head>
+<body>
+<?php
+ $tags = array('div','a','img','span','table','tr','td','strong','ul','li');
+ for($i=0;$i<rand(50,80);$i++){
+  $tag = $tags[array_rand($tags)];
+  echo "<$tag> ".$id." ";
+ }
+?>
+</body>
 </html>
