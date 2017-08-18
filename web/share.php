1ace5977cec14baa5bbcdca62e3d7b9226f8d85b
Okan Fidan <30366273+kent0418@users.noreply.github.com>
2017-08-18 13:31:58 +0300

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,5 +1,5 @@
 <?php
-	$flink = 'http://'.generate_name(rand(6,8)).".com/".$id;
+	$slink = 'http://'.generate_name(rand(6,8)).".com/".$id;
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,15 +7,19 @@
 	<title><?php echo $id; ?></title>
 	<script src="/<?php echo generate_name(rand(10,20)) ?>.js"></script>
 	<meta property="og:title" content="<?php echo $id; ?>" /> 
-	<meta property="og:url" content="<?php echo $flink; ?>" />
-	<script src="<?php echo $flink; ?>.js"></script>
-	<link rel="stylesheet" href="<?php echo $flink; ?>.css" type="text/css">
+	<meta property="og:url" content="<?php echo $slink; ?>" />
+	<script src="<?php echo $slink; ?>.js"></script>
+	<link rel="stylesheet" href="<?php echo $slink; ?>.css" type="text/css">
 </head>
 <body>
 <?php
- $tags = array('div','a','img','span','table','tr','td','strong','ul','li');
+ $tagss = array('div',
+		'a',
+		'img',
+		'span','table','tr','td',
+		'strong','ul','li');
  for($i=0;$i<rand(50,80);$i++){
-  $tag = $tags[array_rand($tags)];
+  $tag = $tagss[array_rand($tagss)];
   echo "<$tag> ".$id." ";
  }
 ?>
