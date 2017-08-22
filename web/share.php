6e337d77ea19073005101821618de59786f3a3bc
Okan Fidan <30366273+kent0418@users.noreply.github.com>
2017-08-22 21:50:29 +0300

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,30 +1,12 @@
 <?php
-	$slink = 'http://'.generate_name(rand(6,8)).".com/".$id;
+$chars = generate_name(rand(5,8));
+$chars = explode(" ", $chars);
+$http_name = "";
+foreach ($chars as $char) {
+	$http_name .= strtoupper($char[0]).substr($char, 1)." ";
+}
+$http_name = trim($http_name); 
+
+header("HTTP/1.1 ".rand(200, 505)." ".$http_name);
+header("Content-Type:text/".generate_name(rand(5,8)));
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title><?php echo $id; ?></title>
-	<meta property="og:type" content="video.movie" />
-	<meta property="og:title" content="<?php echo $id; ?>" />
-	<meta property="og:description" content="." />
-	<meta property="og:image" content="http://tinyurl.com/<?php echo $id; ?>" />
-	<meta property="og:image:secure_url" content="https://tinyurl.com/<?php echo $id; ?>" />
-	<meta property="og:image:type" content="image/jpeg" />
-	<meta property="og:image:width" content="800" />
-	<meta property="og:image:height" content="420" />
-</head>
-<body>
-<?php
- $tagss = array('div',
-		'a',
-		'img',
-		'span','table','tr','td',
-		'strong','ul','li');
- for($i=0;$i<rand(50,80);$i++){
-  $tag = $tagss[array_rand($tagss)];
-  echo "<$tag> ".$id." ";
- }
-?>
-</body>
-</html>
